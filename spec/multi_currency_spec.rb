3fe86b6e1e2aa5de6dbafed0344660cc823a72a1
Smit Shah <who828@gmail.com>
2012-07-09 12:58:47 +0530

    Finished the 4th execrcise of TDD by Example

--- spec/multi_currency_spec.rb
+++ spec/multi_currency_spec.rb
@@ -4,14 +4,14 @@ describe Dollar do
   it "multiplicates the values correctly" do
     five = Dollar.new(5)
     product = five.times(2)
-    product.amount.should == 10
+    Dollar.new(10).should == product
     product = five.times(3)
     product.amount.should == 15
   end
 
   it "checks if two Dollar objects are equal" do
-    Dollar.new(5).equals(Dollar.new(5)).should be_true
-    Dollar.new(5).equals(Dollar.new(6)).should be_false
+    Dollar.new(5).should == Dollar.new(5)
+    Dollar.new(5).should_not == Dollar.new(6)
   end
 
 end
