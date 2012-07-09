99ed913fcbb2306d8004fb06cf0c2af8f47d54e0
Smit Shah <who828@gmail.com>
2012-07-09 13:09:26 +0530

    Renamed the files and refactored the specs

--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -1,12 +1,10 @@
-require 'multi_currency'
+require 'dollar'
 
 describe Dollar do
   it "multiplicates the values correctly" do
     five = Dollar.new(5)
-    product = five.times(2)
-    Dollar.new(10).should == product
-    product = five.times(3)
-    product.amount.should == 15
+    Dollar.new(10).should == five.times(2)
+    Dollar.new(15).should == five.times(3)
   end
 
   it "checks if two Dollar objects are equal" do
